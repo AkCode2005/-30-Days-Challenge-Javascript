e28430230d5c0a7604a4f9f2a4b61cf351036ccc
AkCode2005 <ashkumar2909@gmail.com>
2024-07-11 13:10:22 +0000

    Config Data

---
+++ 01_Basic/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmailId = "ak.fab01@gmail.com"
+var accountPassword = "Ash.2005"
+accountCity = "Delhi"
+// accountId = 2 // not allowed
+
+accountEmailId = "AK.123@gmail.com"
+console.table([accountId,accountEmailId,accountPassword,accountCity])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
