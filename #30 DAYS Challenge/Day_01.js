2ae8ec9b56e302b9dd9e154540e4595fd72be6d1
AkCode2005 <ashkumar2909@gmail.com>
2024-07-13 18:40:40 +0000

    Repo Of Challenge

--- #30 DAYS Challenge/Day_01.js
+++ #30 DAYS Challenge/Day_01.js
@@ -4,9 +4,46 @@
 
 // Task-1
 var studentRollNo = 212345
-console.log("studentRollNo")
-// Task-2
-let studentName= "Ashish"
-console.log("studentName")
+console.log(studentRollNo)
 
+// Task-2
+let studentName = "Ashish"
+console.log(studentName)
+
+//Activity-2
+
+//Task-3
+const studentID = 32456872123
+console.log(studentID)
+
+//Activity-3
+
+//Task-4
+let studentAge = 18
+let FatherName = "Suraj"
+let state = null
+let boolean = true
+ console.log(typeof studentAge)
+ console.log(typeof FatherName)
+ console.log(typeof state)
+ console.log(typeof boolean)
+
+//Activity-4
+
+//Task-5
+let Fruit= "Apple"
+console.log(Fruit)
+Fruit = "Mango"
+console.log(Fruit)
+
+//Activity- 5
+
+//Task-6
+const EmployeeID = 72315
+console.log(EmployeeID)
+// EmployeeID = 21345 // Error - Not able to reassign the value because of const variable 
+//Type Error - Assignment to constant variable.
+
+
+ 
 
